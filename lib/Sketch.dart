3f913b0b32cee9b9cff57af587f666f6700c8a34
nram1234 <redanaser0@gmail.com>
2019-03-13 16:52:10 +0200

    Initial commit

---
+++ lib/Sketch.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+class Sketch extends CustomPainter{
+    List<Offset>ponits;
+
+    Sketch(this.ponits);
+
+    @override
+  void paint(Canvas canvas, Size size) {
+     Paint paint=Paint()..color=Colors.cyanAccent
+         ..strokeCap=StrokeCap.round
+         ..strokeWidth=4.0;
+     for(int i=0;i<ponits.length-1;i++){
+       if(ponits[i]!=null&&ponits[i+1]!=null){
+         canvas..drawLine(ponits[i], ponits[i+1], paint);
+       }
+     }
+  }
+
+  @override
+  bool shouldRepaint(Sketch oldDelegate) {
+
+    return oldDelegate.ponits !=ponits;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
